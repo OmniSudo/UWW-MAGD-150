d5dd36a55d19db7d4b1c244c8b4348f94b5c608c
OmniSudo <omnisudo@gmail.com>
2025-10-08 10:00:29 -0500

    Increase lab5 cool factor by 2.9

--- lab5/sketch.js
+++ lab5/sketch.js
@@ -1,35 +1,34 @@
 function setup() {
-createCanvas(400, 400);
-background(50 );
+    createCanvas(400, 400);
+    background(50);
 }
 
 let theta = 0;
 let color_mul = 1;
 
-function mouseClicked () {
+function mouseClicked() {
     color_mul *= -1;
 }
-
 
 
 function draw() {
     clear()
 
-    background(50 );
+    background(50);
 
-    fill( 150 );
-    circle( 200, 200, 100 ); // POINT
+    fill(150);
+    circle(200, 200, 100); // POINT
 
-    color( 255 )
-    text( "Press a key to move the ship\nClick the mouse to change cone color", 10, 20 )
+    color(255)
+    text("Press a key to move the ship\nClick the mouse to change cone color", 10, 20)
 
-    if ( keyIsPressed ) {
+    if (keyIsPressed) {
         theta = theta + 0.01;
     }
 
-    if ( theta > TWO_PI ) {
+    if (theta > TWO_PI) {
         theta = theta - TWO_PI;
-    } else if ( theta < 0 ) {
+    } else if (theta < 0) {
         theta = TWO_PI + theta;
     }
 
@@ -37,26 +36,40 @@ function draw() {
 
     // ASYMMETRY ( SHIP ON ONE SIDE )
     // SHAPE ( ARC = EXHAUST )
-    translate( 200, 200 );
-    rotate( -theta );
+    translate(200, 200);
+    rotate(-theta);
 
-    push()
+    if ( keyIsPressed) {
         stroke(200, 200, 0)
+
         fill(200, 200, 0)
         arc(150 + 15 / 2.0, 30 + 10, 15, 30, -radians(-90 + 10), -radians(-90 - 10))
+        for (let i = 0; i < 3; ++i) {
+            noStroke()
+            fill(100)
+            push()
+            rotate(i * radians(4) + radians(16))
+            circle(165, 20, (3 - i) * 5)
+            pop()
+        }
+    }
 
-        fill(15);
-        noStroke()
-        triangle(150 + 15 / 2.0, 0 + 25, 150, 25 + 15, 150 + 15, 25 + 15)
+    fill(15);
+    noStroke()
+    triangle(150 + 15 / 2.0, 0 + 25, 150, 25 + 15, 150 + 15, 25 + 15)
 
-        fill(255)
-        quad(150, 0, 150 + 15, 0, 150 + 15, 30, 150, 30)
+    fill(255)
+    quad(150, 0, 150 + 15, 0, 150 + 15, 30, 150, 30)
 
-        fill(0, 200, 200, 100)
-        circle(157.5, 0 + 10, 8)
+    fill(0, 200, 200, 100)
+    circle(157.5, 0 + 10, 8)
 
-        fill(255 + ( color_mul - 1 )/ 2 * 255, 255 - ( color_mul + 1 )/ 2 * 255, 0)
-        triangle(150, 0, 150 + 15, 0, 150 + 15 / 2.0, -15)
-    pop()
+    fill(255 + (color_mul - 1) / 2 * 255, 255 - (color_mul + 1) / 2 * 255, 0)
+    triangle(150, 0, 150 + 15, 0, 150 + 15 / 2.0, -15)
+
+    if ( keyIsPressed) {
+
+    }
+
     pop()
 }
